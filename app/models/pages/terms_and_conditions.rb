d5beba7f99ebea680a68218f6f022b03213fb176
pasha <p.korenev@voroninstudio.eu>
2016-12-16 16:31:04 +0200

    locale

--- app/models/pages/terms_and_conditions.rb
+++ app/models/pages/terms_and_conditions.rb
@@ -1,5 +1,14 @@
 module Pages
   class TermsAndConditions < Cms::Page
-
+    def url(locale = I18n.locale)
+      translation_url = self.translations_by_locale[locale].url
+      if translation_url.blank?
+        return nil
+      end
+      if !translation_url.start_with?("/")
+        translation_url = "/#{translation_url}"
+      end
+      "/#{locale}#{translation_url}"
+    end
   end
 end
